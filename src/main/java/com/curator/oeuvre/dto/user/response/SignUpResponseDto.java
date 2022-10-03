d2a39309f5dbe2efdd3d731218f2f8864f927f15
kim-wonjin <sobaek2000@naver.com>
2022-10-03 17:11:35 +0900

    :rocket: 구글 로그인 API 생성

---
+++ src/main/java/com/curator/oeuvre/dto/user/response/SignUpResponseDto.java
@@ -0,0 +1,29 @@
+package com.curator.oeuvre.dto.user.response;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+public class SignUpResponseDto {
+
+    @ApiModelProperty(example = "1")
+    private final Long userNo;
+
+    @ApiModelProperty(example = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ~~~~")
+    private final String accessToken;
+
+    @ApiModelProperty(example = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ~~~~")
+    private final String refreshToken;
+
+    public SignUpResponseDto(Long userNo, String accessToken, String refreshToken) {
+        this.userNo = userNo;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+}
+
