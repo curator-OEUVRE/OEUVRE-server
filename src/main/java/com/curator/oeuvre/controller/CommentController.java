40bbdc0c09bddcf8622ba621594b9c27d9ef8573
kim-wonjin <sobaek2000@naver.com>
2022-10-22 15:13:03 +0900

    :hammer: 구글로그인 firebase 방식으로 변경

---
+++ src/main/java/com/curator/oeuvre/controller/CommentController.java
@@ -0,0 +1,20 @@
+//package com.curator.oeuvre.controller;
+//
+//import com.curator.oeuvre.service.FloorService;
+//import io.swagger.annotations.Api;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@Slf4j
+//@RequestMapping("/floors")
+//@Api(tags = "06. 방명록 ")
+//@RequiredArgsConstructor
+//@Validated
+//public class FloorController {
+//
+//    private final FloorService floorService;
+//}
